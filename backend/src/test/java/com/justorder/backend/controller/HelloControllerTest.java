61685d3f9cc115e1473eafa75bacf65909515323
Lingfeng555 <lingfeng.chen@opendeusto.es>
2026-03-12 15:59:04 +0100

    Sample test as a template holder

---
+++ backend/src/test/java/com/justorder/backend/controller/HelloControllerTest.java
@@ -0,0 +1,21 @@
+package com.justorder.backend.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class MiControllerTest{
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testEndpoint() throws Exception {
+        mockMvc.perform(get("/api/hello"))
+               .andExpect(status().isOk());
+    }
+}
