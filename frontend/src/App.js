11c01fc61cecb0eb0a21103b5beedb2f65357959
sararegalado <sara.regalado@opendeusto.es>
2026-03-15 16:48:52 +0100

    frontend restaurant registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
 import RegisterCustomer from './pages/RegisterCustomer';
+import RegisterRestaurant from './pages/RegisterRestaurant';
 
 import './styles/Register.css'; 
+import './styles/Home.css';
+
 
 function App() {
     return (
-    <Router>
-        <Routes>
-            {/* Main page */}
-            <Route path="/" element={<Navigate to="/register-customer" />} />
-            {/* URL for customer registration */}
-            <Route path="/register-customer" element={<RegisterCustomer />} />
-        </Routes>
-    </Router>
-  );
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/register-customer" element={<RegisterCustomer />} />
+                <Route path="/register-restaurant" element={<RegisterRestaurant />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
