d3aa342d27d21dc6850ab063f71402f7b5365740
Ibonerdoiza <ibon.erdoiza@opendeusto.es>
2026-03-18 13:56:38 +0100

    Add new tests

--- backend/src/main/java/com/justorder/backend/controller/AllergenController.java
+++ backend/src/main/java/com/justorder/backend/controller/AllergenController.java
@@ -3,6 +3,7 @@ package com.justorder.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +22,10 @@ public class AllergenController {
     // GET /api/allergens returns a list containing all the allergens
     @GetMapping
     public ResponseEntity<List<AllergenDTO>> getAllAllergens() {
-        return ResponseEntity.ok(allergenService.getAllAllergens());
+        try {
+            return ResponseEntity.ok(allergenService.getAllAllergens());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
