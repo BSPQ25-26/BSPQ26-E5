a03e4052eb58b1e0432cb3a9a0c76ddfa10524c0
IgorBarredo <igor.barredo@opendeusto.es>
2026-05-08 12:06:11 +0200

    fix: forzar a GitHub Actions a leer el codigo actualizado

--- backend/src/test/java/com/justorder/backend/controller/RestaurantControllerTest.java
+++ backend/src/test/java/com/justorder/backend/controller/RestaurantControllerTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
