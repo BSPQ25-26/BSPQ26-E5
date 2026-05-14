e8a72be21f55499433e443ba37999ff2910dc345
IgorBarredo <igor.barredo@opendeusto.es>
2026-05-14 07:50:54 +0200

    unnecessary lines deleted

--- backend/src/test/java/com/justorder/backend/controller/HelloControllerTest.java
+++ backend/src/test/java/com/justorder/backend/controller/HelloControllerTest.java
@@ -3,25 +3,15 @@ package com.justorder.backend.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.justorder.backend.security.JwtAuthenticationFilter;
-import com.justorder.backend.security.JwtUtil;
 
 @WebMvcTest(HelloController.class)
 class HelloControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private JwtAuthenticationFilter jwtAuthenticationFilter;
-
-    @Autowired
-    private JwtUtil jwtUtil;
 
     @Test
     void testEndpoint() throws Exception {
