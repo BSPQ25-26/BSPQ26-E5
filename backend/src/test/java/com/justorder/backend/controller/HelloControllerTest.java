05adbb74a04316c87442cdb8a47b0290e826085c
IgorBarredo <igor.barredo@opendeusto.es>
2026-05-14 16:42:23 +0200

    hello controler test cheked

--- backend/src/test/java/com/justorder/backend/controller/HelloControllerTest.java
+++ backend/src/test/java/com/justorder/backend/controller/HelloControllerTest.java
@@ -2,12 +2,15 @@ package com.justorder.backend.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(HelloController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class HelloControllerTest {
 
     @Autowired
